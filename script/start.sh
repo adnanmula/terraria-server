18f1c77e34af638b87f5a4be158bd53f62cb36fe
adnan <adnanalfonso.mula@pccomponentes.com>
2019-07-09 20:56:03 +0200

    update config.json

--- script/start.sh
+++ script/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mono --server --gc=sgen -O=all TerrariaServer.exe -world /server/world/01.wld -configpath "/server/config" -worldpath "/server/world/01.wld"
+mono --server --gc=sgen -O=all TerrariaServer.exe -world /server/world/01.wld -configpath "/server/config" -worldpath "/server/world"
