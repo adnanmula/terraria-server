41cabf334da3d5aa88fdc57fed204b615ab15cc6
adnan <adnanalfonso.mula@pccomponentes.com>
2019-07-09 21:05:40 +0200

    enable world autocreation

--- script/start.sh
+++ script/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mono --server --gc=sgen -O=all TerrariaServer.exe -world /server/world/01.wld -configpath "/server/config" -worldpath "/server/world"
+mono --server --gc=sgen -O=all TerrariaServer.exe -autocreate 2 -world /server/world/world.wld -configpath "/server/config" -worldpath "/server/world"
